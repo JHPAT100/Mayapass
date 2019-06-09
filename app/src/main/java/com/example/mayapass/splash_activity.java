3e99d4599458e26b6b3a99773936f8ff9c8fbd80
Dionicio Yam <Dionicioyam@gmail.com>
2019-06-09 18:00:01 -0500

    se agregaron Imagenes

---
+++ app/src/main/java/com/example/mayapass/splash_activity.java
@@ -0,0 +1,21 @@
+package com.example.mayapass;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class splash_activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
